da0da2a0d3dcf18596a7e38cc039c166aee14365
Sreeni <37336941+dssreeni@users.noreply.github.com>
2018-03-14 20:02:24 +0530

    Uploading list
    Uploading list

---
+++ python/list.py
@@ -0,0 +1,20 @@
+#Lists are editable, 
+#Lists will be inferred by [] symbol.
+
+list1 = [10,20,30]
+print(type(list1))
+
+list2 = ['abc', 10, True]
+
+#access list elements by slicing
+print(list1[0])
+print(list1[0:3])
+print(list1[2:3])
+
+list1[2] = 100
+print(list1)
+
+list1.append(10)
+
+list3 = [list1, 60, list2]
+len(list3)
